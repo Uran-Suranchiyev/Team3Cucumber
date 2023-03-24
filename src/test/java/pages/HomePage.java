334eaee73a2a80ff704a4efcfc228fb0be1d12cf
Uran-Suranchiyev <uransura@gmail.com>
2023-03-23 21:05:06 -0400

    ssssss

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+public class HomePage {
+    public HomePage(){
+        PageFactory.initElements(BrowserUtils.getDriver(), this);
+    }
+}
---
+++ src/test/java/step_definitions/HomeSteps.java
@@ -0,0 +1,10 @@
+package step_definitions;
+
+import pages.HomePage;
+
+public class HomeSteps {
+    HomePage page;
+    public HomeSteps(){
+        page = new HomePage();
+    }
+}
